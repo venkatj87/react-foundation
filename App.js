a80b4d06fcf9122f1cdcda12e87902f08d6bda55
venkatj87 <venkatj87@outlook.com>
2024-02-22 21:48:36 +0000

    Parcel installed and learn deep about it

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const element = React.createElement (
+    'h2', 
+    {id: "heading"},
+    React.createElement (
+        "span", 
+        {},
+        [
+            React.createElement (
+                "h1", 
+                {},
+                "Some h1 tag inside span"
+            ),
+            React.createElement (
+                "h2", 
+                {},
+                "Some h2 tag inside span"
+            )
+        ]
+    )
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(element);
+console.log(element)
