d3a6657159c8a7129875b83da67f713fec120700
venkatj87 <venkatj87@outlook.com>
2024-02-22 18:21:55 +0000

    Basic APP

---
+++ App.js
@@ -0,0 +1,23 @@
+const element = React.createElement (
+    'h2', 
+    {id: "heading"},
+    React.createElement (
+        "span", 
+        {},
+        [
+            React.createElement (
+                "h1", 
+                {},
+                "Some h1 tag inside span"
+            ),
+            React.createElement (
+                "h2", 
+                {},
+                "Some h2 tag inside span"
+            )
+        ]
+    )
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(element);
+console.log(element)
